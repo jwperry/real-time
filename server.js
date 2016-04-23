6f970e7e26431d3a0996fc559d28708cd0e6cece
Joe Perry <joseph.w.perry@gmail.com>
2016-04-23 15:25:06 -0600

    new.html view added, changed welcome screen

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const io = socketIo(server);
 const countVotes = require('./lib/count-votes');
 const pollId = require('./lib/poll-id');
 
-var votes = {};
+app.locals.title = 'CrowdSource'
+app.locals.votes = {};
 
 server.listen(port, function() {
   console.log('Listening on port ' + port + '.');
@@ -17,14 +18,18 @@ server.listen(port, function() {
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/home.html');
+  res.sendFile(__dirname + '/public/welcome.html');
+});
+
+app.get('/new', function(req, res){
+  res.sendFile(__dirname + '/public/new.html');
 });
 
 io.on('connection', function(socket) {
   console.log('A user has connected. Active users: ' + io.engine.clientsCount + '.');
   io.sockets.emit('connectedUsers', io.engine.clientsCount);
   socket.emit('statusMessage', 'You have connected.');
-  socket.emit('voteCount', countVotes(votes));
+  socket.emit('voteCount', countVotes(app.locals.votes));
 
   socket.on('disconnect', function() {
     console.log('A user has disconnected. Active users: ' + io.engine.clientsCount + '.');
@@ -33,9 +38,9 @@ io.on('connection', function(socket) {
 
   socket.on('message', function(channel, message) {
     if (channel === 'voteCast') {
-      votes[socket.id] = message;
+      app.locals.votes[socket.id] = message;
       var confirmMessage = 'You have cast your vote for: ' + message + '.';
-      io.sockets.emit('voteCount', countVotes(votes));
+      io.sockets.emit('voteCount', countVotes(app.locals.votes));
       socket.emit('voteConfirm', confirmMessage);
     }
   });
